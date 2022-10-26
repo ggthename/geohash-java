3a235a469a031f03c20ebf1d21a0a7bddccf6017
yoonwon.ko <1qazxsw@3e>
2022-10-26 11:34:19 +0900

    -bounding box연산을 위하여 각 geohash level의 격자 width, height 저장

--- src/main/java/org/my/GeoHashLevel.java
+++ src/main/java/org/my/GeoHashLevel.java
@@ -1,23 +1,26 @@
 package org.my;
-
 public enum GeoHashLevel {
-    ONE(2,3, 1),
-    TWO(5,5, 2),
-    THREE(7,8, 3),
-    FOUR(10,10, 4),
-    FIVE(12,13, 5),
-    SIX(15,15, 6),
-    SEVEN(17,18, 7),
-    EIGHT(20,20, 8);
+    ONE(2,3, 1, 5000, 5000),
+    TWO(5,5, 2, 1250*1000, 625* 1000),
+    THREE(7,8, 3, 156 * 1000, 156* 1000),
+    FOUR(10,10, 4, 39.1 * 1000, 39.1 * 1000 ),
+    FIVE(12,13, 5, 4.89*1000, 4.89*1000),
+    SIX(15,15, 6, 1.22*1000, 0.61*1000),
+    SEVEN(17,18, 7, 153, 153),
+    EIGHT(20,20, 8, 38.2, 19.1);
 
     private int latitudeBitsLength;
     private int longitudeBitsLength;
     private int stringLength;
+    private double cellWidthMeter;
+    private double cellHeightMeter;
 
-    GeoHashLevel(int latBits, int lngBits, int stringLength){
+    GeoHashLevel(int latBits, int lngBits, int stringLength, double cellWidthMeter, double cellHeightMeter){
         this.latitudeBitsLength =latBits;
         this.longitudeBitsLength =lngBits;
         this.stringLength = stringLength;
+        this.cellWidthMeter = cellWidthMeter;
+        this.cellHeightMeter = cellHeightMeter;
     }
 
     public int getLoopCount(GcsType geoHashType){
@@ -34,4 +37,11 @@ public enum GeoHashLevel {
         return this.stringLength;
     }
 
+    public double getCellHeightMeter() {
+        return cellHeightMeter;
+    }
+
+    public double getCellWidthMeter() {
+        return cellWidthMeter;
+    }
 }
